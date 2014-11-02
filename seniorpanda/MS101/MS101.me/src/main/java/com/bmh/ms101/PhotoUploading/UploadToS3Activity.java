21a4d081849cfeb65fe6d41d40a33b345b5bcdc6
Steve Wang <sw211@duke.edu>
2014-11-02 00:34:28 -0400

    merge conflict

---
+++ seniorpanda/MS101/MS101.me/src/main/java/com/bmh/ms101/PhotoUploading/UploadToS3Activity.java
@@ -0,0 +1,14 @@
+package main.java.com.bmh.ms101.PhotoUploading;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Hadoop on 11/1/14.
+ */
+public class UploadToS3Activity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
